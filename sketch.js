110357c36ddb5e5206d4e09461e6eb0f15adb2e4
KishaBhatia <dhallseema@yahoo.com>
2020-07-19 22:11:45 +0530

    MARIO GAME

--- sketch.js
+++ sketch.js
@@ -4,11 +4,14 @@ var game;
 var obstacles,obstacle_img,obstaclesGroup;
 var player,player_img;
 var ground;
+var coins,coin_img,coinsGroup;
+var count=0;
 
 function preload(){
   track_img=loadImage("finalImages/ground.png");
   obstacle_img=loadImage("finalImages/pipes.png");
   player_img=loadImage("finalImages/player.png");
+  coin_img=loadImage("finalImages/coin.png")
 }
 
 function setup() {
@@ -16,6 +19,7 @@ function setup() {
   game=new Game();
 
   obstaclesGroup=new Group();
+  coinsGroup=new Group();
 
   for(var i=0;i<displayWidth*5;i=i+300){
     obstacles=createSprite(i,360,30,100);
@@ -23,6 +27,14 @@ function setup() {
     obstacles.scale=0.5;
     obstaclesGroup.add(obstacles);
     obstacles.setCollider("rectangle",0,0,obstacles.width-100,obstacles.height-100);
+  }
+
+  for(var j=0;j<displayWidth*5;j=j+200){
+    coins=createSprite(j,random(200,300),30,100);
+    coins.addImage("coins",coin_img);
+    coins.scale=0.8;
+    coinsGroup.add(coins);
+    //coins.setCollider("rectangle",0,0,coins.width,coins.height);
   }
 
   ground=createSprite(displayWidth-20,displayHeight/2+80);
@@ -41,8 +53,13 @@ function draw() {
   game.play();
 
   player.collide(ground);
-  player.collide(obstaclesGroup);
-
+  player.collide(obstaclesGroup)
+  
+  if(player.isTouching(coinsGroup)){
+    //playSound("coin.wav");
+    count=count+10;
+    //console.log(count);
+  }
   if(keyIsDown(RIGHT_ARROW)){
     player.x=player.x+5;
   }
@@ -51,10 +68,13 @@ function draw() {
   }
   if(keyIsDown(32)){
     player.velocityY=-7;
+    //playSound("jump.wav");
   }
   player.velocityY=player.velocityY+0.5;
-  /*if(keyIsDown(DOWN_ARROW)){
-    player.y=player.y+5;
-  }*/
+ 
+  textSize(30);
+  textFont("Comic Sans MS");
+  text("Score:"+count,displayWidth-200,displayHeight/2-300);
+
   drawSprites();
 }
